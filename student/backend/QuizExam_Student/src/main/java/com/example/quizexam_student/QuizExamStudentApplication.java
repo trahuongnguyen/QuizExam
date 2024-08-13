0498c2f243da06eff6e124d81b0ea8e0a23173a1
Nguyễn Quang Huy <nqhuy2092004@gmail.com>
2024-08-13 10:39:09 +0700

    Design Base

---
+++ student/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/QuizExamStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizexam_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizExamStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizExamStudentApplication.class, args);
+    }
+
+}
