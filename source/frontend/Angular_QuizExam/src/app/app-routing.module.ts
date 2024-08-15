629b4d242459438d2e0b2d3b173c55041e95431e
Nguyễn Quang Huy <nqhuy2092004@gmail.com>
2024-08-15 22:08:13 +0700

    create header, footer homepage
    Lỗi logo, chưa hoạt động đc nút switch light-dark

--- source/frontend/Angular_QuizExam/src/app/app-routing.module.ts
+++ source/frontend/Angular_QuizExam/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './student/login/login.component';
+import { HeaderComponent } from './student/home/common/header/header.component';
+import { FooterComponent } from './student/home/common/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/footer', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- source/frontend/Angular_QuizExam/src/app/app.module.ts
+++ source/frontend/Angular_QuizExam/src/app/app.module.ts
@@ -6,8 +6,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-
+import { LoginComponent } from './student/login/login.component';
 
 @NgModule({
   declarations: [
