220c976bfd2e44c6428affd270f498516c65d5ea
Nguyễn Quang Huy <nqhuy2092004@gmail.com>
2024-08-20 12:40:02 +0700

    update structure homepage file #2

--- source/frontend/Angular_QuizExam/src/app/student/home/home.module.ts
+++ source/frontend/Angular_QuizExam/src/app/student/home/home.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from '../../app-routing.module'; 
 import { HomepageComponent } from './homepage/homepage.component';
 import { ProfileComponent } from './profile/profile.component';
 import { MarkComponent } from './mark/mark.component';
-
 
 
 @NgModule({
@@ -13,7 +15,14 @@ import { MarkComponent } from './mark/mark.component';
     MarkComponent
   ],
   imports: [
+    BrowserModule,
+    AppRoutingModule,
     CommonModule
-  ]
+  ],
+  providers: [
+    provideHttpClient(),
+    provideClientHydration()
+  ],
+  bootstrap: []
 })
 export class HomeModule { }
