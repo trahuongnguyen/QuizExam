26e5e43bd4a5a50fec40e02c013ed2371f1dbe80
thuhuyen1703 <huyenhanhhiep123@gmail.com>
2024-08-24 15:56:04 +0700

    user_profile

--- source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
+++ source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  visiblePassword(value: string): void {
+    const passwordInput = document.getElementById(value) as HTMLInputElement;
+    if (passwordInput) {
+      const type = passwordInput.type === "password" ? "text" : "password";
+      passwordInput.type = type;
+    }
+  }
 }
