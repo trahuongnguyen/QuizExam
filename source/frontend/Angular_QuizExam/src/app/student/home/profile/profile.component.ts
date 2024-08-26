ec50c13b37e08b91ac8269b8ab6d08bbc0d44750
thuhuyen1703 <huyenhanhhiep123@gmail.com>
2024-08-26 12:56:50 +0700

    fix change password profile

--- source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
+++ source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { profile } from 'node:console';
 
 @Component({
   selector: 'app-profile',
@@ -6,11 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  visiblePassword(value: string): void {
-    const passwordInput = document.getElementById(value) as HTMLInputElement;
+  // Hàm để ẩn/hiện mật khẩu
+  visiblePassword(inputId: string): void {
+    // Tìm phần tử đầu vào mật khẩu bằng ID
+    const passwordInput = document.getElementById(inputId) as HTMLInputElement;
+
     if (passwordInput) {
-      const type = passwordInput.type === "password" ? "text" : "password";
-      passwordInput.type = type;
+      // Kiểm tra và thay đổi thuộc tính 'type' giữa 'password' và 'text'
+      const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+      passwordInput.setAttribute("type", type);
+    } else {
+      console.error(`Element with id ${inputId} not found.`);
     }
   }
 }
+
