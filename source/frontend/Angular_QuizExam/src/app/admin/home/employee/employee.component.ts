eff51bbae03fd441967b8e6627ff1fd7e4616528
Nguyễn Quang Huy <nqhuy2092004@gmail.com>
2024-08-29 12:38:45 +0700

    call api ListEmployee

--- source/frontend/Angular_QuizExam/src/app/admin/home/employee/employee.component.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/home/employee/employee.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
+  responseData: any;
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.http.get<any>(`${this.authService.apiUrl}user`).subscribe(
+      (data) => {
+        this.responseData = data;// Log dữ liệu nhận được từ API
+      },
+      (error) => {
+      }
+    );
+  }
 }
