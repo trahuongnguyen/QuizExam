f24832a6f0fb926435be9dc387a4c1162b66092a
Nguyễn Duy Đại <ngduydai21@gmail.com>
2024-08-29 11:38:48 +0700

    fix profile, change module switch mode button

--- source/frontend/Angular_QuizExam/src/app/admin/home/home.component.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   title = 'Angular_QuizExam';
   thisRouter = '/admin/home#'
   windowScrolled = false;
@@ -16,54 +17,5 @@ export class HomeComponent implements OnInit {
     this.windowScrolled = Math.round(window.scrollY) !=0;
   }
 
-  darkMode: boolean = false;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    this.loadTheme();
-  }
-
-  // Kiểm tra xem localStorage có sẵn không trước khi sử dụng
-  private isLocalStorageAvailable(): boolean {
-    return typeof localStorage !== 'undefined';
-  }
-
-  saveTheme(isDarkMode: boolean): void {
-    if (this.isLocalStorageAvailable()) {
-      localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-      this.updateBodyClass(isDarkMode);
-    }
-  }
-
-  // Tải trạng thái chế độ từ localStorage
-  loadTheme(): void {
-    if (this.isLocalStorageAvailable()) {
-      const theme = localStorage.getItem('theme');
-      this.darkMode = theme === 'dark';
-      this.updateBodyClass(this.darkMode);
-    }
-  }
-
-  // Cập nhật lớp CSS
-  updateBodyClass(isDarkMode: boolean): void {
-    const body = document.getElementById('body');
-    if (body) {
-      if (isDarkMode) {
-        body.setAttribute('QuizTech-theme', 'dark');
-      } else {
-        body.setAttribute('QuizTech-theme', 'light');
-      }
-    }
-  }
-
-  toggleTheme(): void {
-    this.darkMode = !this.darkMode;
-    this.saveTheme(this.darkMode);
-  }
-
-  menuToggle(): void {
-    const toggleMenu = document.querySelector(".menu");
-    toggleMenu?.classList.toggle("active");
-  }
+  constructor(public app : AppComponent) {}
 }
