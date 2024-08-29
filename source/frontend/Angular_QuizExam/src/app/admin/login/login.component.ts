b6397a1f29d4874ff64ccafba08fd8c31f168c32
thuhuyen1703 <huyenhanhhiep123@gmail.com>
2024-08-29 12:28:50 +0700

    login admin

--- source/frontend/Angular_QuizExam/src/app/admin/login/login.component.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/login/login.component.ts
@@ -1,45 +1,15 @@
-import { Component } from '@angular/core';
-import {Swiper} from 'swiper';
-import 'swiper/swiper-bundle.css'; 
+import { Component, Inject, Injectable, OnInit } from '@angular/core';
+// import 'swiper/swiper-bundle.css'; // Thêm dòng này vào
+import Swiper from 'swiper';
+import { DOCUMENT } from '@angular/common';
+import { Navigation, Pagination } from 'swiper/modules';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
-  initializeSwiper(){
-          // Lấy các phần tử DOM
-    const progressCircle = document.querySelector<SVGElement>(".autoplay-progress svg");
-    const progressContent = document.querySelector<HTMLSpanElement>(".autoplay-progress span");
-
-    // Kiểm tra xem các phần tử có tồn tại trước khi sử dụng chúng
-    if (progressCircle && progressContent) {
-        // Tạo Swiper
-        const swiper = new Swiper(".mySwiper", {
-            spaceBetween: 30,
-            centeredSlides: true,
-            autoplay: {
-                delay: 2500,
-                disableOnInteraction: false
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev"
-            },
-            on: {
-                autoplayTimeLeft(s: any, time: number, progress: number) {
-                    progressCircle.style.setProperty("--progress", `${1 - progress}`);
-                    progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-                }
-            }
-        });
-    }
-
-    }
 }
 
