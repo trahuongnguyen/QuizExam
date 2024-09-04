f6ee9935c0e6a9c5764e5983f5939a5c81825b4b
Nguyễn Duy Đại <ngduydai21@gmail.com>
2024-09-04 21:13:51 +0700

    update active sidebar

--- source/frontend/Angular_QuizExam/src/app/admin/home/home.component.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../../app.component';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,7 @@ import { AppComponent } from '../../app.component';
 })
 export class HomeComponent {
   title = 'Angular_QuizExam';
-  thisRouter = '/admin/home#'
+  thisRouter = '/admin/home'
   windowScrolled = false;
   scrollToTop(): void {
     window.scrollTo(0, 0);
@@ -17,5 +19,18 @@ export class HomeComponent {
     this.windowScrolled = Math.round(window.scrollY) !=0;
   }
 
-  constructor(public app : AppComponent) {}
+  constructor(public app : AppComponent, private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.currentRoute = this.router.url;
+      console.log(this.currentRoute)
+    });
+  }
+
+  currentRoute: string = '';
+
+  isActive(route: string): boolean {
+    return this.currentRoute ===  this.thisRouter + route;
+  }
 }
