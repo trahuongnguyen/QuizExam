5cc4f3912510fcdc4c03f8fe04a313466227a368
thuandd221100 <thuan221100>
2024-09-09 10:16:44 +0700

    sort file export api

--- source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/controller/UserController.java
+++ source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.quizexam_student.controller;
 
 import com.example.quizexam_student.bean.request.PasswordRequest;
+import com.example.quizexam_student.bean.request.UserRequest;
 import com.example.quizexam_student.bean.response.EmpExcelExporter;
 import com.example.quizexam_student.bean.response.EmpPDFExporter;
 import com.example.quizexam_student.bean.response.UserResponse;
 import com.example.quizexam_student.entity.Role;
+import com.example.quizexam_student.entity.User;
 import com.example.quizexam_student.exception.EmptyException;
+import com.example.quizexam_student.service.ExportService;
 import com.example.quizexam_student.service.RoleService;
 import com.example.quizexam_student.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -33,6 +37,7 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     private final RoleService roleService;
+    private final ExportService exportService;
 
     @GetMapping("")
     public List<UserResponse> getAll(){
@@ -66,33 +71,63 @@ public class UserController {
         return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
     }
 
-//    @GetMapping("/export/excel")
-//    public ResponseEntity<String> exportToExcel(HttpServletResponse response) throws IOException {
-//        response.setContentType("application/octet-stream");
-//        String headerKey = "Content-Disposition";
-//        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-//        String currentDateTime = dateFormat.format(new Date());
-//        String fileName = "users_" + currentDateTime + ".xlsx";
-//        String headerValue = "attachment; filename=" + fileName;
-//        response.setHeader(headerKey, headerValue);
-//        List<UserResponse> users = userService.getUserByRolePermission();
-//        EmpExcelExporter excelExporter = new EmpExcelExporter(users);
-//        excelExporter.export(response);
-//        return new ResponseEntity<>("Export To Excel Successfully", HttpStatus.OK);
-//    }
-//
-//    @GetMapping(value = "/export/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-//    public ResponseEntity<String> exportToPDF(HttpServletResponse response) throws IOException {
-//        response.setContentType("application/pdf");
-//        String headerKey = "Content-Disposition";
-//        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-//        String currentDateTime = dateFormat.format(new Date());
-//        String fileName = "users_" + currentDateTime + ".pdf";
-//        String headerValue = "attachment; filename=" + fileName;
-//        response.setHeader(headerKey, headerValue);
-//        List<UserResponse> users = userService.getUserByRolePermission();
-//        EmpPDFExporter userPDFExporter = new EmpPDFExporter(users);
-//        userPDFExporter.export(response);
-//        return new ResponseEntity<>("Export To PDF Successfully", HttpStatus.OK);
-//    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('DIRECTOR') or hasRole('SRO')  or hasRole('TEACHER')")
+    @GetMapping("/update/{id}")
+    public User updateUser(@PathVariable int id) {
+        return userService.findById(id);
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('DIRECTOR') or hasRole('SRO') or hasRole('TEACHER')")
+    @PostMapping("/update/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable int id, @RequestBody @Valid UserRequest userRequest) {
+        userService.updateUser(id, userRequest);
+        return new ResponseEntity("Update User Success", HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('DIRECTOR') or hasRole('SRO')  or hasRole('TEACHER') or hasRole('STUDENT')")
+    @GetMapping("/changePassword/{id}")
+    public UserResponse getProfile(@PathVariable int id){
+        return userService.getUserById(id);
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('DIRECTOR') or hasRole('SRO')  or hasRole('TEACHER') or hasRole('STUDENT')")
+    @PostMapping("/changePassword/{id}")
+    public ResponseEntity<String> updateProfile(@PathVariable int id, @RequestBody @Valid PasswordRequest passwordRequest) {
+        userService.changePassword(id, passwordRequest);
+        return new ResponseEntity<>("Password updated successfully", HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('DIRECTOR') or hasRole('SRO')")
+    @GetMapping("/export/excel")
+    public ResponseEntity<String> exportToExcel(HttpServletResponse response) throws IOException {
+        exportService.export(response, "user", "xlsx");
+        String email = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        Role role = userService.findUserByEmail(email).getRole();
+        List<Role> roles = roleService.findAllByPermission(role.getId());
+        List<UserResponse> users = new ArrayList<>();
+        roles.forEach(role1 -> {
+            users.addAll(userService.getUserByRolePermission(role1));
+        });
+        EmpExcelExporter excelExporter = new EmpExcelExporter(users);
+        excelExporter.export(response);
+        return new ResponseEntity<>("Export To Excel Successfully", HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('DIRECTOR') or hasRole('SRO')")
+    @GetMapping(value = "/export/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<String> exportToPDF(HttpServletResponse response) throws IOException {
+        exportService.export(response, "user", "pdf");
+        String email = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        Role role = userService.findUserByEmail(email).getRole();
+        List<Role> roles = roleService.findAllByPermission(role.getId());
+        List<UserResponse> users = new ArrayList<>();
+        roles.forEach(role1 -> {
+            users.addAll(userService.getUserByRolePermission(role1));
+        });
+        EmpPDFExporter userPDFExporter = new EmpPDFExporter(users);
+        userPDFExporter.export(response);
+        return new ResponseEntity<>("Export To PDF Successfully", HttpStatus.OK);
+    }
+
 }
