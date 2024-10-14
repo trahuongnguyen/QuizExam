78187ec95bda129bfeb062393b6f61cb1d8af93f
Nguyễn Duy Đại <ngduydai21@gmail.com>
2024-10-14 15:30:45 +0700

    fix table

--- source/frontend/Angular_QuizExam/src/app/student/student.component.ts
+++ source/frontend/Angular_QuizExam/src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 
@@ -6,6 +6,57 @@ import { Component } from '@angular/core';
   templateUrl: './student.component.html',
   styleUrl: './student.component.css'
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit {
   title = 'Angular_QuizExam';
+
+  darkMode: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loadTheme();
+  }
+
+  // Kiểm tra xem localStorage có sẵn không trước khi sử dụng
+  private isLocalStorageAvailable(): boolean {
+    return typeof localStorage !== 'undefined';
+  }
+
+  saveTheme(isDarkMode: boolean): void {
+    if (this.isLocalStorageAvailable()) {
+      localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+      this.updateBodyClass(isDarkMode);
+    }
+  }
+
+  // Tải trạng thái chế độ từ localStorage
+  loadTheme(): void {
+    if (this.isLocalStorageAvailable()) {
+      const theme = localStorage.getItem('theme');
+      this.darkMode = theme === 'dark';
+      this.updateBodyClass(this.darkMode);
+    }
+  }
+
+  // Cập nhật lớp CSS
+  updateBodyClass(isDarkMode: boolean): void {
+    const body = document.getElementById('body');
+    if (body) {
+      if (isDarkMode) {
+        body.setAttribute('QuizTech-theme', 'dark');
+      } else {
+        body.setAttribute('QuizTech-theme', 'light');
+      }
+    }
+  }
+
+  toggleTheme(): void {
+    this.darkMode = !this.darkMode;
+    this.saveTheme(this.darkMode);
+  }
+
+  menuToggle(): void {
+    const toggleMenu = document.querySelector(".menu");
+    toggleMenu?.classList.toggle("active");
+  }
 }
