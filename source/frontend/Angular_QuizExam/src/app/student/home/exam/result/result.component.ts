963a9c8e8f267dc348300f7a269ca202e3d5b324
Nguyễn Duy Đại <ngduydai21@gmail.com>
2024-10-31 16:20:20 +0700

    update result

--- source/frontend/Angular_QuizExam/src/app/student/home/exam/result/result.component.ts
+++ source/frontend/Angular_QuizExam/src/app/student/home/exam/result/result.component.ts
@@ -4,165 +4,162 @@ import { AuthService } from '../../../service/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { HomeComponent } from '../../home.component';
 import { ExamComponent } from '../exam.component';
-import { ToastrService } from 'ngx-toastr';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrl: './result.component.css'
+  styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
-  examId: number = 0;
-  subjectName: String = '';
+  examId: number = 0; // ID của bài kiểm tra
 
-  listLevel: any;
+  subjectName: string = ''; // Tên môn học
+  listLevel: any; // Danh sách các cấp độ
 
-  score: number = 0;
-  maxScore: number = 0;
+  score: number = 0; // Điểm của sinh viên
+  maxScore: number = 0; // Điểm tối đa
+  
+  scoreByLevel: any; // Điểm theo từng cấp độ
+  maxScoreByLevel: any; // Điểm tối đa theo từng cấp độ
+  percentageScoreByLevel: any; // Tỉ lệ điểm theo cấp độ
 
-  scoreByLevel: any;
-  maxScoreByLevel: any;
-  percentagesList: any;
-
-  timeTaken: any;
+  timeTaken: string = ''; // Thời gian làm bài
 
   constructor(
     private authService: AuthService,
     private http: HttpClient,
     private home: HomeComponent,
     private examComponent: ExamComponent,
-    private router: Router,
-    private toastr: ToastrService,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    // Lấy examId từ route
     this.examId = Number(this.activatedRoute.snapshot.paramMap.get('examId'));
 
     const markRequest = this.http.get<any>(`${this.authService.apiUrl}/mark/${this.examId}`, this.home.httpOptions);
     const levelRequest = this.http.get<any>(`${this.authService.apiUrl}/question-record/max-score-level/${this.examId}`, this.home.httpOptions);
     const scoreByLevelRequest = this.http.get<any>(`${this.authService.apiUrl}/student-answers/score-level/${this.examId}`, this.home.httpOptions);
 
+    // Thực hiện các yêu cầu đồng thời
     forkJoin([markRequest, levelRequest, scoreByLevelRequest]).subscribe(([markData, levelData, scoreByLevelData]) => {
-      // Lấy dữ liệu bảng mark
+      // Lưu dữ liệu bài kiểm tra
       this.examComponent.mark = markData;
-
-      // Lấy danh sách tên level
-      this.listLevel = Object.keys(levelData);
+      this.listLevel = Object.keys(levelData); // Lấy danh sách các cấp độ
       this.maxScoreByLevel = levelData;
-
-      // Lấy điểm cho từng level của student
       this.scoreByLevel = scoreByLevelData;
 
+      // Lấy tên môn học và điểm
       this.subjectName = this.examComponent.mark.subjectName;
       this.score = this.examComponent.mark.score;
       this.maxScore = this.examComponent.mark.maxScore;
-
+      
       // Tính toán thời gian làm bài
-      const submittedTime = new Date(this.examComponent.mark.submittedTime).getTime();
-      const beginTime = new Date(this.examComponent.mark.beginTime).getTime();
-      this.timeTaken = submittedTime - beginTime;
-
-      // Chuyển đổi thời gian làm bài thành phút và giây
-      const timeTakenInMinutes = Math.floor(this.timeTaken / 60000);
-      const timeTakenInSeconds = Math.floor((this.timeTaken % 60000) / 1000);
-
-      // Chuyển đổi định dạng "mm:ss"
-      const formattedMinutes = timeTakenInMinutes < 10 ? '0' + timeTakenInMinutes : timeTakenInMinutes;
-      const formattedSeconds = timeTakenInSeconds < 10 ? '0' + timeTakenInSeconds : timeTakenInSeconds;
-
-      this.timeTaken = `${formattedMinutes}:${formattedSeconds}`;
-
-      const percentageByLevel: Record<string, string> = {};
-
-      for (const level of this.listLevel) {
-        const score = this.scoreByLevel[level] || 0; // Lấy điểm hoặc 0 nếu không có
-        const maxScore = this.maxScoreByLevel[level] || 1; // Tránh chia cho 0
-        const percentage = maxScore > 0 ? (score / maxScore) * 100 : 0; // Tránh chia cho 0
-        percentageByLevel[level] = percentage.toFixed(2); // Lưu kết quả với 2 chữ số thập phân
-      }
-      console.log("Tỷ lệ phần trăm theo level:", percentageByLevel);
-
-      // Chuyển đổi thành mảng số
-      this.percentagesList = Object.values(percentageByLevel).map(value => parseFloat(value));
-
+      this.calculateTimeTaken(this.examComponent.mark.submittedTime, this.examComponent.mark.beginTime);
+      
+      // Tính toán tỉ lệ điểm theo cấp độ
+      this.calculatePercentageByLevel();
+      
+      // Vẽ biểu đồ
       this.drawCharts();
     });
   }
 
+  calculateTimeTaken(submittedTime: string, beginTime: string): void {
+    // Tính toán thời gian làm bài và chuyển đổi thành định dạng mm:ss
+    const timeTakenInMs = new Date(submittedTime).getTime() - new Date(beginTime).getTime();
+    const minutes = Math.floor(timeTakenInMs / 60000);
+    const seconds = Math.floor((timeTakenInMs % 60000) / 1000);
+    this.timeTaken = `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+  }
+
+  calculatePercentageByLevel(): void {
+    // Tính toán tỉ lệ điểm cho từng cấp độ
+    const percentageByLevel = this.listLevel.reduce((total: any, level: any) => {
+      const score = this.scoreByLevel[level] || 0;
+      const maxScore = this.maxScoreByLevel[level] || 1; // Tránh chia cho 0
+      total[level] = ((score / maxScore) * 100).toFixed(2);
+      return total;
+    }, {});
+    this.percentageScoreByLevel = Object.values(percentageByLevel).map(Number); // Chuyển đổi thành số
+  }
+
   drawCharts(): void {
-    // Logic để thay đổi màu dựa trên giá trị điểm
-    const backgroundColors = this.percentagesList.map((score: any) => {
-      if (score < 30) {
-        return '#EE0906'; // Màu đỏ cho điểm dưới 30
-      }
-      else if (score >= 30 && score <= 70) {
-        return '#FFD912'; // Màu vàng cho điểm từ 30 đến 70
-      }
-      else {
-        return '#26AA10'; // Màu xanh cho điểm trên 70
-      }
-    });
-    const borderColors = backgroundColors;
+    this.drawDoughnutChart();
+    this.drawBarChart();
+  }
 
-    const ctx = document.getElementById('progressChart') as HTMLCanvasElement;
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: this.listLevel,
-        datasets: [{
-          data: this.percentagesList,
-          backgroundColor: backgroundColors,
-          borderColor: borderColors,
-          borderWidth: 1,
-          barThickness: 30 // Độ dày của thanh
-        }]
-      },
-      options: {
-        indexAxis: 'y',  // Chuyển thành biểu đồ ngang
-        scales: {
-          x: {
-            beginAtZero: true,
-            max: 100
-          }
-        },
-        plugins: {
-          legend: {
-            display: false // Ẩn nhãn "Scores" ở đầu biểu đồ
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: false
-      }
-    });
-
-    const ctx2 = document.getElementById('doughnutChart') as HTMLCanvasElement;
-    // Tạo biểu đồ Doughnut Chart
-    new Chart(ctx2, {
+  drawDoughnutChart(): void {
+    // Vẽ biểu đồ Doughnut
+    const doughnutChart = new Chart(document.getElementById('doughnutChart') as HTMLCanvasElement, {
       type: 'doughnut',
       data: {
         labels: ['Score', 'Remain Score'],
-        datasets: [
-          {
-            data: [this.score, this.maxScore - this.score], // Tính toán điểm
-            backgroundColor: ['#26AA10', '#DDDDDD'], // Màu phần đạt và phần chưa đạt
-            borderWidth: 0,
-          }
-        ]
+        datasets: [{
+          data: [0, this.maxScore], // Khởi đầu với điểm 0
+          backgroundColor: ['#26AA10', '#DDDDDD'],
+          borderWidth: 0,
+        }]
       },
       options: {
-        cutout: '50%', // Làm trống giữa vòng tròn
+        cutout: '50%',
         responsive: true,
         maintainAspectRatio: false,
         plugins: {
-          tooltip: { enabled: true }, // Ẩn tooltip
-          legend: { display: false }   // Ẩn chú thích
+          tooltip: { enabled: true },
+          legend: { display: false }
         },
-        animation: {
-          animateRotate: false
-        }
+        animation: { animateRotate: false, animateScale: false }
+      }
+    });
+
+    let currentScore = 0;
+    const totalFrames = 20; // 1 giây với 20 fps
+    const scoreIncrement = this.score / totalFrames;
+
+    const updateChart = () => {
+      currentScore = Math.min(currentScore + scoreIncrement, this.score);
+      doughnutChart.data.datasets[0].data = [currentScore, this.maxScore - currentScore];
+      doughnutChart.update();
+    
+      // Cập nhật phần trăm vào phần tử HTML
+      const percentage = Math.floor((currentScore / this.maxScore) * 100);
+      document.getElementById('percentageLabel')!.innerText = `${percentage}%`;
+    
+      if (currentScore < this.score) {
+        requestAnimationFrame(updateChart);
+      }
+    };
+    requestAnimationFrame(updateChart);
+  }
+
+  drawBarChart(): void {
+    // Vẽ biểu đồ cột
+    const barColors = this.percentageScoreByLevel.map((score: any) => {
+      if (score < 30) {
+        return '#EE0906'; // Đỏ cho điểm dưới 30%
+      }
+      if (score <= 70) {
+        return '#FFD912'; // Vàng cho điểm từ 30% đến 70%
+      }
+      return '#26AA10'; // Xanh cho điểm trên 70%
+    });
+
+    new Chart(document.getElementById('progressChart') as HTMLCanvasElement, {
+      type: 'bar',
+      data: {
+        labels: this.listLevel,
+        datasets: [{ data: this.percentageScoreByLevel, backgroundColor: barColors, barThickness: 30 }]
+      },
+      options: {
+        indexAxis: 'y',
+        scales: { x: { beginAtZero: true, max: 100 } },
+        plugins: { legend: { display: false } },
+        animation: { duration: 2000 },
+        responsive: true,
+        maintainAspectRatio: false
       }
     });
   }
