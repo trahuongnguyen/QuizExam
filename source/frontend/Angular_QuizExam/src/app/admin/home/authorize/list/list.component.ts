db802e9a4808d27b341f7df19a944a5ca283ff4f
Nguyễn Quang Huy <nqhuy2092004@gmail.com>
2024-11-06 09:29:15 +0700

    authorize, fix student list, all btn export

--- source/frontend/Angular_QuizExam/src/app/admin/home/authorize/list/list.component.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/home/authorize/list/list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { AuthorizeComponent } from './../authorize.component';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../service/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { HomeComponent } from '../../home.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  constructor(private authService: AuthService, public home: HomeComponent, private http: HttpClient, public toastr: ToastrService, private router: Router, public authorizeComponent: AuthorizeComponent) { }
 
 }
